b9ed2c956c4e7eb8db71c8bc56e2ad38e93a6593
Eric Cheong <144624528+eric-c-uoft@users.noreply.github.com>
2024-11-30 18:35:03 -0500

    Cleans up some of the edit text GUI + add TODO's (#23)
    * Update EditTextView.java
    
    * Update TextAreaView.java
    
    * Update EditTextView.java

--- src/main/java/view/TextBoxView/EditTextView.java
+++ src/main/java/view/TextBoxView/EditTextView.java
@@ -5,19 +5,17 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class EditTextView extends JPanel implements ActionListener {
-    final private JPanel textPanel;
     final JButton saveButton = new JButton("Save");
 
     final JButton deleteButton = new JButton("Delete");
 
-    final JButton closeButton = new JButton("Cancel");
     TextAreaView textEditPanel;
 
     public EditTextView() {
         // TODO implement data storage to grab constructor values
         textEditPanel = new TextAreaView("", "");
 
-        textPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        JPanel buttonPanel1 = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
 
         // configuring buttons
         saveButton.setSize(150, 75);
@@ -29,41 +27,23 @@ public class EditTextView extends JPanel implements ActionListener {
         deleteButton.addActionListener(this);
 
         // Setting up the view
-        textPanel.add(saveButton, BorderLayout.EAST);
+        buttonPanel1.add(saveButton, BorderLayout.EAST);
+        buttonPanel1.add(deleteButton, BorderLayout.WEST);
 
-        textPanel.add(deleteButton, BorderLayout.WEST);
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 1, 1)); // Align buttons to the right
-
-        // configuring buttons
-        saveButton.setPreferredSize(new Dimension(75, 30));
-        saveButton.setForeground(Color.GREEN.darker());
-        saveButton.addActionListener(this);
-
-        closeButton.setPreferredSize(new Dimension(75, 30));
-        closeButton.setForeground(Color.RED);
-        closeButton.addActionListener(this);
-
-        // Adding buttons to the panel
-        buttonPanel.add(saveButton);
-        buttonPanel.add(closeButton);
-
-        // Setting up the view
         setLayout(new BorderLayout());
-        add(textEditPanel, BorderLayout.NORTH);
-        add(textPanel, BorderLayout.CENTER);
-
         add(textEditPanel, BorderLayout.NORTH); // Text area view at the top
-        add(buttonPanel, BorderLayout.SOUTH); // Button panel at the bottom (aligned right)
+        add(buttonPanel1, BorderLayout.CENTER); // Button panel at the bottom (aligned right)
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == saveButton) {
+            // TODO implement confirmation message for overriding 
             saveText();
         }
 
         else if (e.getSource() == deleteButton) {
-
+            // TODO implement confirmation message for deleting
         }
     }
 
