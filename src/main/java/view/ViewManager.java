6cc16a7df64c4fff5bf7e343fad0e33124b658b5
Sebastien Psarianos <sebastien@psarianos.com>
2024-12-02 22:04:38 -0500

    fixed issue with view manager

--- src/main/java/view/ViewManager.java
+++ src/main/java/view/ViewManager.java
@@ -25,7 +25,6 @@ public class ViewManager implements PropertyChangeListener {
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        System.out.println(evt.getNewValue());
         if (evt.getPropertyName().equals("state")) {
             final String viewModelName = (String) evt.getNewValue();
             cardLayout.show(views, viewModelName);
