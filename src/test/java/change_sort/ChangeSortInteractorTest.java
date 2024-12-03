c3e90712a61e61b2a3b0fad1f03b7a36320b1339
bowlofcupramen <djhjiang321@gmail.com>
2024-12-03 17:56:03 -0500

    Finished test cases.

--- src/test/java/change_sort/ChangeSortInteractorTest.java
+++ src/test/java/change_sort/ChangeSortInteractorTest.java
@@ -21,21 +21,22 @@ class ChangeSortInteractorTest {
         Entry entry3 = data.createEntry();
 
         entry1.setDate("1733187600"); // Dec 3rd
-        entry2.setDate("1733014800"); // Dec 1st
+        entry2.setDate("0"); // Epoch start
         entry3.setDate("1733101200"); // Dec 2nd
 
-        EntryListButtonDataComparitor comparitor = new EntryListButtonDataComparitor(SortMethod.DATE_ASCENDING);
+        EntryListButtonDataComparitor comparator = new EntryListButtonDataComparitor(SortMethod.DATE_ASCENDING);
         ArrayList<EntryListButtonData> list = new ArrayList<>();
         list.add(new EntryListButtonData(entry1));
         list.add(new EntryListButtonData(entry2));
         list.add(new EntryListButtonData(entry3));
-        list.sort(comparitor);
+        list.sort(comparator);
 
         ChangeSortOutputBoundary presenter = new ChangeSortOutputBoundary() {
             @Override
             public void prepareSuccessView(ChangeSortOutputData outputData) {
-                assertEquals(list.getFirst().getDate(), "1733014800");
+                assertEquals(list.getFirst().getDate(), "0");
                 assertEquals("1733187600", list.getLast().getDate());
+                assertEquals(entry2.getId(), list.getFirst().getId()); // Entry 3 has ID 3
             }
 
             @Override
@@ -78,6 +79,36 @@ class ChangeSortInteractorTest {
     }
 
     @Test
+    void successSortDateDescendingTest() {
+        EntryDataAccess data = new EntryDataAccess();
+        ChangeSortInputData changeSortInputData = new ChangeSortInputData(SortMethod.DATE_DESCENDING);
+        Entry entry1 = data.createEntry();
+        Entry entry2 = data.createEntry();
+
+        entry2.setTitle("a");
+        entry1.setTitle("Z");
+        EntryListButtonDataComparitor comparator = new EntryListButtonDataComparitor(SortMethod.DATE_DESCENDING);
+        ArrayList<EntryListButtonData> list = new ArrayList<>();
+        list.add(new EntryListButtonData(entry1));
+        list.add(new EntryListButtonData(entry2));
+        list.sort(comparator);
+
+        ChangeSortOutputBoundary presenter = new ChangeSortOutputBoundary() {
+            @Override
+            public void prepareSuccessView(ChangeSortOutputData outputData) {
+                assertEquals("Z", list.getFirst().getTitle());
+            }
+
+            @Override
+            public void prepareFailView(String errorMessage) {
+                fail("Use case success is unexpected.");
+            }
+        };
+        ChangeSortInputBoundary interactor = new ChangeSortInteractor(data, presenter);
+        interactor.execute(changeSortInputData);
+    }
+
+    @Test
     public void successSameTitleTest() {
         EntryDataAccess data = new EntryDataAccess();
         ChangeSortInputData changeSortInputData = new ChangeSortInputData(SortMethod.TITLE_DESCENDING);
@@ -92,12 +123,12 @@ class ChangeSortInteractorTest {
         entry1.setDate("1733101200"); // Dec 2nd
         entry3.setDate("1733187600"); // Dec 3rd
 
-        EntryListButtonDataComparitor comparitor = new EntryListButtonDataComparitor(SortMethod.TITLE_DESCENDING);
+        EntryListButtonDataComparitor comparator = new EntryListButtonDataComparitor(SortMethod.TITLE_DESCENDING);
         ArrayList<EntryListButtonData> list = new ArrayList<>();
         list.add(new EntryListButtonData(entry1));
         list.add(new EntryListButtonData(entry2));
         list.add(new EntryListButtonData(entry3));
-        list.sort(comparitor);
+        list.sort(comparator);
 
         ChangeSortOutputBoundary presenter = new ChangeSortOutputBoundary() {
             @Override
