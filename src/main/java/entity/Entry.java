6cd5ceca1832e82c2c98794d4471071aaa7ad7f9
Sebastien Psarianos <sebastien@psarianos.com>
2024-12-02 21:32:08 -0500

    typo

--- src/main/java/entity/Entry.java
+++ src/main/java/entity/Entry.java
@@ -80,7 +80,7 @@ public class Entry {
      * @param path The image path of the newly added image
      * @return The new image id of this entry
      */
-    public voidgi addImagePath(String path) {
+    public void addImagePath(String path) {
         imagePaths.put(newImgId, path);
         newImgId++;
     }
